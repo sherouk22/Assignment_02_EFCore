92f24ca197d84d7f2d1e5aee99530962a47e139c
sherouk22 <sheroukmohy558@gmail.com>
2025-03-02 02:25:18 +0200

    Add project files.

---
+++ Assignment_02_EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_02_EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
